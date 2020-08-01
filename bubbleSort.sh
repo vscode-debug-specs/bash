281dba5d5768d264a156e2342adafc43491f6ed2
74th <site@74th.tech>
2020-08-01 11:57:09 +0900

    update for .vscode

--- bubbleSort.sh
+++ bubbleSort.sh
@@ -2,28 +2,25 @@
 
 function bubble_sort() {
 	list=()
-	for x in ${@}
-	do
+	for x in ${@}; do
 		list+=(${x})
 	done
 
 	i=0
-	while [ ${i} -lt ${#list[*]} ]
-	do
-		jMax=`expr ${#list[*]} - ${i} - 1`
+	while [ ${i} -lt ${#list[*]} ]; do
+		jMax=$(expr ${#list[*]} - ${i} - 1)
 		j=0
-		while [ ${j} -lt ${jMax} ]
-		do
-			jp=`expr $j + 1`
+		while [ ${j} -lt ${jMax} ]; do
+			jp=$(expr $j + 1)
 			left=${list[${j}]}
 			right=${list[${jp}]}
-			if [ $left -ge $right ] ; then
+			if [ $left -ge $right ]; then
 				list[$j]=$right
 				list[$jp]=$left
 			fi
-			j=`expr $j + 1`
+			j=$(expr $j + 1)
 		done
-		i=`expr $i + 1`
+		i=$(expr $i + 1)
 	done
 	echo ${list[@]}
 }
